60fe6841d6975cf08a7a4e402976404d41607324
mmmfang <mae.fang@gmail.com>
2021-03-19 04:05:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+    let i;
+    let arr = [];
+    for (i=0, i< musicians.length, i++) {
+      arr.push(`musicians[i] plays instruments[i]`);
+    } return arr;
+}
